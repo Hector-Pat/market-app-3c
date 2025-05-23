9a14011a849fa063dee4e03a44db2b7a79f07a41
Hector-Pat <enderpat223@gmail.com>
2025-05-23 13:27:41 -0600

    Two simple entities added

---
+++ src/main/java/com/tecdesoftware/market_app_3c/entity/Compras.java
@@ -0,0 +1,83 @@
+package com.tecdesoftware.market_app_3c.entity;
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="compras")
+public class Compra {
+    @Id//ES LA LLAVE PRIMARIA
+    //Auto genera ids utoincrementables
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id_compra")
+    private int idCompra;
+    private String nombre;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    private Integer comentario;
+
+    private Boolean estado;
+
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(String medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public Integer getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(Integer comentario) {
+        this.comentario = comentario;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/tecdesoftware/market_app_3c/entity/Producto.java
@@ -0,0 +1,85 @@
+package com.tecdesoftware.market_app_3c.entity;
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="productos")
+public class Producto {
+    @Id//ES LA LLAVE PRIMARIA
+    //Auto genera ids utoincrementables
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name="id_Producto")
+    private int idProducto;
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name="codigo_barras")
+    private String codigoBarras;
+
+    @Column(name="precio_venta")
+    private Double precioVenta;
+
+    @Column(name="cantidad_stocks")
+    private Integer cantidadStocks;
+    private Boolean estado;
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStocks() {
+        return cantidadStocks;
+    }
+
+    public void setCantidadStocks(Integer cantidadStocks) {
+        this.cantidadStocks = cantidadStocks;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
+
